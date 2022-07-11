828b9522591973111bcaf60af1062bbf28470bcb
Darío <dariogarcigarci@gmail.com>
2022-07-11 13:29:13 +0200

    Formularios template terminados

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  terminosCondiciones: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
